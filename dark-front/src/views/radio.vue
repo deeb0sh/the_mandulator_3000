2c2b4a6185f8b16fb5f3e3165a7a9eec4a75bb0b
deeb0sh <deeb0sh@ya.ru>
2025-01-25 10:57:33 +0300

    +StreamUrlJson

--- dark-front/src/views/radio.vue
+++ dark-front/src/views/radio.vue
@@ -12,7 +12,7 @@
                 </div>
         </div>
         <div>
-            <button v-on:click="SetStream('mnml')">Minimal Deep Techno</button><br>
+            <button v-on:click="SetStream('minimal')">Minimal Deep Techno</button><br>
             <button v-on:click="SetStream('dnb')">Drum and Bass</button><br>
             <button v-on:click="SetStream('techno')">Techno</button><br>
             <button v-on:click="SetStream('dubtechno')">Dub Techno</button><br>
@@ -35,7 +35,6 @@
             <button v-on:click="SetStream('idm')">IDM</button><br>
         </div>
     </div>
-    {{ url }}
     <Player v-bind:Source="url"/>
 </template>
 <script>
@@ -47,74 +46,93 @@ export default {
     },
     methods: {
             SetStream(tmp) {
-                console.log(tmp)
-                
-                if (tmp === "mnml") {
-                    this.url = "http://amoris.sknt.ru:80/minimal"
-                } 
-                else if (tmp === "dnb") {
-                    this.url = "http://amoris.sknt.ru:80/dnb"
+                const StreamUrls = { 
+                    "minimal": "http://amoris.sknt.ru:80/minimal",
+                    "dnb": "http://amoris.sknt.ru:80/dnb",
+                    "techno": "http://amoris.sknt.ru:80/techno",
+                    "dubtechno": "http://amoris.sknt.ru:80/dubtechno",
+                    "triphop": "http://amoris.sknt.ru:80/triphop",
+                    "deeptech": "http://amoris.sknt.ru:80/deeptech",
+                    "ambient": "http://amoris.sknt.ru:80/ambient",
+                    "dubstep": "http://amoris.sknt.ru:80/dubstep",
+                    "goa": "http://amoris.sknt.ru:80/goa",
+                    "newage": "http://amoris.sknt.ru:80/newage",
+                    "dubtechnomix": "http://amoris.sknt.ru:80/dubtechnomix",
+                    "electro": "http://amoris.sknt.ru:80/electro",
+                    "electronic": "http://amoris.sknt.ru:80/electronic",
+                    "breakbeat": "http://amoris.sknt.ru:80/breakbeat",
+                    "breaks": "http://amoris.sknt.ru:80/breaks",
+                    "breakcore": "http://amoris.sknt.ru:80/breakcore",
+                    "dub": "http://amoris.sknt.ru:80/dub",
+                    "reggae": "http://amoris.sknt.ru:80/reggae",
+                    "djpepe": "http://amoris.sknt.ru:80/djpepe",
+                    "trance": "http://amoris.sknt.ru:80/trance",
+                    "idm": "http://amoris.sknt.ru:80/idm",
                 }
-                else if (tmp === "techno") {
-                    this.url = "http://amoris.sknt.ru:80/techno"
-                }
-                else if (tmp === "dubtechno") {
-                    this.url = "http://amoris.sknt.ru:80/dubtechno"
-                }
-                else if (tmp === "triphop") {
-                    this.url = "http://amoris.sknt.ru:80/triphop"
-                }
-                else if (tmp === "deeptech") {
-                    this.url = "http://amoris.sknt.ru:80/deeptech"
-                }
-                else if (tmp === "ambient") {
-                    this.url = "http://amoris.sknt.ru:80/ambient"
-                }
-                else if (tmp === "dubstep") {
-                    this.url = "http://amoris.sknt.ru:80/dubstep"
-                }
-                else if (tmp === "goa") {
-                    this.url = "http://amoris.sknt.ru:80/goa"
-                }
-                else if (tmp === "newage") {
-                    this.url = "http://amoris.sknt.ru:80/newage"
-                }
-                else if (tmp === "dubtechnomix") {
-                    this.url = "http://amoris.sknt.ru:80/dubtechnomix"
-                }
-                else if (tmp === "electro") {
-                    this.url = "http://amoris.sknt.ru:80/electro"
-                }
-                else if (tmp === "electronic") {
-                    this.url = "http://amoris.sknt.ru:80/electronic"
-                }
-                else if (tmp === "breakbeat") {
-                    this.url = "http://amoris.sknt.ru:80/breakbeat"
-                }
-                else if (tmp === "breaks") {
-                    this.url = "http://amoris.sknt.ru:80/breaks"
-                }
-                else if (tmp === "breakcore") {
-                    this.url = "http://amoris.sknt.ru:80/breakcore"
-                }
-                else if (tmp === "dub") {
-                    this.url = "http://amoris.sknt.ru:80/dub"
-                }
-                else if (tmp === "reggae") {
-                    this.url = "http://amoris.sknt.ru:80/reggae"
-                }
-                else if (tmp === "djpepe") {
-                    this.url = "http://amoris.sknt.ru:80/djpepe"
-                }
-                else if (tmp === "trance") {
-                    this.url = "http://amoris.sknt.ru:80/trance"
-                }
-                else if (tmp === "idm") {
-                    this.url = "http://amoris.sknt.ru:80/idm"
-                }
-
-
-                console.log(this.url)
+                this.url = StreamUrls[tmp]
+                // if (tmp === "minimal") {
+                //     this.url = "http://amoris.sknt.ru:80/minimal"
+                // } 
+                // else if (tmp === "dnb") {
+                //     this.url = "http://amoris.sknt.ru:80/dnb"
+                // }
+                // else if (tmp === "techno") {
+                //     this.url = "http://amoris.sknt.ru:80/techno"
+                // }
+                // else if (tmp === "dubtechno") {
+                //     this.url = "http://amoris.sknt.ru:80/dubtechno"
+                // }
+                // else if (tmp === "triphop") {
+                //     this.url = "http://amoris.sknt.ru:80/triphop"
+                // }
+                // else if (tmp === "deeptech") {
+                //     this.url = "http://amoris.sknt.ru:80/deeptech"
+                // }
+                // else if (tmp === "ambient") {
+                //     this.url = "http://amoris.sknt.ru:80/ambient"
+                // }
+                // else if (tmp === "dubstep") {
+                //     this.url = "http://amoris.sknt.ru:80/dubstep"
+                // }
+                // else if (tmp === "goa") {
+                //     this.url = "http://amoris.sknt.ru:80/goa"
+                // }
+                // else if (tmp === "newage") {
+                //     this.url = "http://amoris.sknt.ru:80/newage"
+                // }
+                // else if (tmp === "dubtechnomix") {
+                //     this.url = "http://amoris.sknt.ru:80/dubtechnomix"
+                // }
+                // else if (tmp === "electro") {
+                //     this.url = "http://amoris.sknt.ru:80/electro"
+                // }
+                // else if (tmp === "electronic") {
+                //     this.url = "http://amoris.sknt.ru:80/electronic"
+                // }
+                // else if (tmp === "breakbeat") {
+                //     this.url = "http://amoris.sknt.ru:80/breakbeat"
+                // }
+                // else if (tmp === "breaks") {
+                //     this.url = "http://amoris.sknt.ru:80/breaks"
+                // }
+                // else if (tmp === "breakcore") {
+                //     this.url = "http://amoris.sknt.ru:80/breakcore"
+                // }
+                // else if (tmp === "dub") {
+                //     this.url = "http://amoris.sknt.ru:80/dub"
+                // }
+                // else if (tmp === "reggae") {
+                //     this.url = "http://amoris.sknt.ru:80/reggae"
+                // }
+                // else if (tmp === "djpepe") {
+                //     this.url = "http://amoris.sknt.ru:80/djpepe"
+                // }
+                // else if (tmp === "trance") {
+                //     this.url = "http://amoris.sknt.ru:80/trance"
+                // }
+                // else if (tmp === "idm") {
+                //     this.url = "http://amoris.sknt.ru:80/idm"
+                // }
                 setTimeout(() => {  
                         document.getElementById('player').play(); 
                 }, 250);
