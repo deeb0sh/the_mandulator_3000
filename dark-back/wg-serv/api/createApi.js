37fbe41ec94b257505645c09e43954f01cc8e590
deeb0sh <deeb0sh@ya.ru>
2025-04-21 18:26:56 +0300

    save all 436356356562323

--- dark-back/wg-serv/api/createApi.js
+++ dark-back/wg-serv/api/createApi.js
@@ -6,191 +6,182 @@ import syncwg from '../utils/syncwg.js'
 import defIp from '../utils/defaultIp.js'
 import crypto from 'crypto'
 
-
 export default async function wgCreateApi(fastify) {
-    fastify.register(jwt, { // регистарция jwt плагина
-        secret: process.env.JWT_SECRET // секрет в.env JWT_SECRET=""
+    fastify.register(jwt, {
+        secret: process.env.JWT_SECRET
     })
 
     fastify.post('/wg/create', {
         schema: {
-                headers: headersJwtValid, // валидация хедеров один JWT
-                body: wgNameValid // валидация тела запрпса
-            }
-        },
-        async (request, reply) => {
-            try {
-                const decod = await request.jwtVerify() //первое валидация токена их хедера
-                const user = decod.user // логин на мандуляторе
-                // const role = decod.role // роль на мандуляторе
-                const { wguser, location } = request.body // имя впн-пользователя и локаицмя
-                const currentUser = await fastify.prisma.users.findFirst({
-                    where: { 
-                           login: user
-                    },
-                    select: { 
-                        id: true,
-                        subnets: {
-                           where: {
-                              serverName: location // Фильтруем подсети по выбранному серверу
-                            },
-                            select: {
-                                network: true,
-                                serverName: true
-                            }
-                        },
-                        role: {
-                            select: {
-                                name: true,
-                                speed: true,
-                                network: true
-                            }
-                        }
-                      }
-                    })
+            headers: headersJwtValid,
+            body: wgNameValid
+        }
+    },
+    async (request, reply) => {
+        try {
+            const decod = await request.jwtVerify()
+            fastify.log.info(`JWT верифицирован: ${JSON.stringify(decod)}`)
+            
+            const user = decod.user
+            const { wguser, location } = request.body
+            fastify.log.info(`Создание WG клиента: ${wguser}, локация: ${location}, пользователь: ${user}`)
 
-                // ip сервера чтобы не назначить его пользователю
-                const wgIp = await defIp(fastify, location) 
-                
-                // вычесляем ip впн-пользователя исходя из его роли сеть / ip 
-                const userSubnet = currentUser.subnets[0] // ок
-                const userNet = userSubnet.network.replace(/\/.*/, `/${currentUser.role.network}`); // переписываем после "/" (меняем сидр)
-                const [ip, mask] = userNet.split("/") // всё понтяно
-                const ipCount = 2 ** (32 - Number(mask)) // Количество ip адресов в маске
-                let [oct1, oct2, oct3, oct4] = ip.split(".").map(Number) // разбиваем ip на октеты и приобразуем в числа
-                let ipRangeServ = [] // инициализируем массив в которм будут все доступные ip адереса пользователя (без broadcast)
-                for (let i = 1; i < ipCount - 1; i++) {  // +1 и -1 исключаем броадкаст
-                    ipRangeServ.push(`${oct1}.${oct2}.${oct3}.${oct4 + i}/${mask}`); // генерируем список доступных ip на ноде
-                }
-
-                const ipRange = ipRangeServ.filter(ip => ip !== (wgIp + "/28")) // если находим в массиве с ip , ip шлюза WG , то исключаем его
-
-                // ищем все используемые ip в локации пользователя
-                const usedIp = await fastify.prisma.client.findMany({
-                    where: {
-                        user: {
-                          login: user // логин пользователя
-                        },
-                        serverName: location // выбранная локация (например, 'DE', 'RU', 'FI')
-                    },
-                    select: {
-                        ip: true,
-      
-                    }
-                })
-     
-                const usedIpsSet = new Set(usedIp.map(client => client.ip.split("/")[0])); // Разделяем на IP и маску
-                const availableIps = ipRange.filter(ip => !usedIpsSet.has(ip.split("/")[0]));
-              
-                if (availableIps.length === 0) {
-                    return reply.send({ onErr: "лимит локации"})
-                } 
-                const firstFreeIp = availableIps[0];
-                
-                const privateKey = crypto.randomBytes(32).toString('base64') // Приватный ключ
-                const publicKey = crypto.createHash('sha256').update(privateKey).digest('base64') // Публичный ключ
-                
-                const servers = {
-                  "RU": "ru.darksurf.ru",
-                  "DE": "de.darksurf.ru",
-                  "FI": "fi.darksurf.ru"
-                }
-                const endPoint = servers[location]
-
-                const server = await fastify.prisma.server.findFirst({
-                    where: {
-                        serverName: location
-                    },
-                    select: {
-                        id: true
-                    }            
-                })
-
-                const newWgClient = {
-                    name: wguser,
-                    ip: firstFreeIp,
-                    privateKey: privateKey,
-                    publicKey: publicKey,
-                    endPoint: endPoint,
-                    serverName: location,
-                    serverId: server.id,
-                    userId: currentUser.id
-                }
-                
-                await fastify.prisma.client.create({
-                    data: newWgClient
-                });
-                await syncwg(fastify, location) // когда всё созданова отправляем информацию на wg-сервер
-                return reply.send({ message: "valid"})
-            }
-            catch (onErr) {
-                return reply.send({ 
-                    message: "invalid",
-                    onErr: `ошибка! ${onErr}`,
-                })
-            }
-        })
-
-        fastify.delete('/wg/create', {
-            schema: {
-                    headers: headersJwtValid, // валидация хедеров один JWT
-                    body: wgid // валидация тела запрпса
-                }
-            },
-            async (request, reply) => {
-                try{
-                    const decod = await request.jwtVerify() //первое валидация токена их хедера
-                    const user = decod.user // логин на мандуляторе
-                    const { id } = request.body // забераем id из тема после валидаций
-                    
-                    // узнаём id учётки на мандуляторе (логина)
-                    const login = await fastify.prisma.users.findFirst({ 
-                        where: {
-                            login: user
-                        },
+            const currentUser = await fastify.prisma.users.findFirst({
+                where: { login: user },
+                select: {
+                    id: true,
+                    subnets: {
+                        where: { serverName: location },
                         select: {
-                            id: true
-                        }
-                    })
-                    const loginId = login.id
-                    // проверяс есть принадлезнасть клиента к пользователю
-                    const checkClient = await fastify.prisma.client.findFirst({ 
-                        where: {
-                            userId: Number(loginId),
-                            id: Number(id)
-                        },
-                        select: {
-                            name: true
-                        }
-                    })
-                    // усли попытка подмены тогда инвалид
-                    if (!checkClient) {
-                        return reply.send({ message: "invalid", onErr: "запрещено, Клиент не пренадлежит пользователю"})
-                    }
-                    // определяем в какой локации клиент
-                    const loc = await fastify.prisma.client.findFirst({
-                        where: {
-                            id: Number(id)
-                        },
-                        select: {
+                            network: true,
                             serverName: true
                         }
-                    })
-
-                    // если всё ок удаляем клиента
-                    await fastify.prisma.client.delete({
-                        where: {
-                            id: Number(id) // delete только по одному параментру (призма)
-                         }
-                    })
-                    await syncwg(fastify, loc.serverName) // отрплвяем новые данные на сервер
-
-                    return reply.send({ message: "valid"})
+                    },
+                    role: {
+                        select: {
+                            name: true,
+                            speed: true,
+                            network: true
+                        }
+                    }
                 }
-                catch (e) {
-                    return reply.send({ message: "invalid", e})
-                }
+            })
+
+            fastify.log.info(`Пользователь из БД: ${JSON.stringify(currentUser)}`)
+
+            const wgIp = await defIp(fastify, location)
+            fastify.log.info(`IP сервера (исключаемый): ${wgIp}`)
+
+            const userSubnet = currentUser.subnets[0]
+            const userNet = userSubnet.network.replace(/"/g, '').replace(/\/.*/, `/${currentUser.role.network}`)
+            fastify.log.info(`Подсеть пользователя после коррекции маски: ${userNet}`)
+
+            const [ip, mask] = userNet.split("/")
+            const ipCount = 2 ** (32 - Number(mask))
+            let [oct1, oct2, oct3, oct4] = ip.split(".").map(Number)
+            let ipRangeServ = []
+
+            for (let i = 1; i < ipCount - 1; i++) {
+                ipRangeServ.push(`${oct1}.${oct2}.${oct3}.${oct4 + i}/${mask}`)
             }
-        )
 
+            const ipRange = ipRangeServ.filter(ip => ip !== (wgIp + "/28"))
+            fastify.log.info(`Доступные IP после исключения шлюза: ${JSON.stringify(ipRange)}`)
+
+            const usedIp = await fastify.prisma.client.findMany({
+                where: {
+                    user: { login: user },
+                    serverName: location
+                },
+                select: {
+                    ip: true
+                }
+            })
+
+            const usedIpsSet = new Set(usedIp.map(client => client.ip.split("/")[0]))
+            const availableIps = ipRange.filter(ip => !usedIpsSet.has(ip.split("/")[0]))
+
+            fastify.log.info(`Свободные IP: ${JSON.stringify(availableIps)}`)
+
+            if (availableIps.length === 0) {
+                return reply.send({ onErr: "лимит локации" })
+            }
+
+            const firstFreeIp = availableIps[0]
+            const privateKey = crypto.randomBytes(32).toString('base64')
+            const publicKey = crypto.createHash('sha256').update(privateKey).digest('base64')
+
+            const servers = {
+                "RU": "ru.darksurf.ru",
+                "DE": "de.darksurf.ru",
+                "FI": "fi.darksurf.ru"
+            }
+            const endPoint = servers[location]
+
+            const server = await fastify.prisma.server.findFirst({
+                where: { serverName: location },
+                select: { id: true }
+            })
+
+            const newWgClient = {
+                name: wguser,
+                ip: firstFreeIp,
+                privateKey,
+                publicKey,
+                endPoint,
+                serverName: location,
+                serverId: server.id,
+                userId: currentUser.id
+            }
+
+            fastify.log.info(`Создаём нового клиента WG: ${JSON.stringify(newWgClient)}`)
+
+            await fastify.prisma.client.create({ data: newWgClient })
+            await syncwg(fastify, location)
+
+            return reply.send({ message: "valid" })
+        }
+        catch (onErr) {
+            fastify.log.error(`Ошибка при создании WG клиента: ${onErr}`)
+            return reply.send({ 
+                message: "invalid",
+                onErr: `ошибка! ${onErr}`
+            })
+        }
+    })
+
+    fastify.delete('/wg/create', {
+        schema: {
+            headers: headersJwtValid,
+            body: wgid
+        }
+    },
+    async (request, reply) => {
+        try {
+            const decod = await request.jwtVerify()
+            const user = decod.user
+            fastify.log.info(`Запрос на удаление WG клиента пользователем: ${user}`)
+
+            const { id } = request.body
+            fastify.log.info(`ID клиента на удаление: ${id}`)
+
+            const login = await fastify.prisma.users.findFirst({
+                where: { login: user },
+                select: { id: true }
+            })
+
+            const loginId = login.id
+            const checkClient = await fastify.prisma.client.findFirst({
+                where: {
+                    userId: Number(loginId),
+                    id: Number(id)
+                },
+                select: { name: true }
+            })
+
+            if (!checkClient) {
+                fastify.log.warn(`Попытка удалить чужого клиента: ${id} пользователем: ${user}`)
+                return reply.send({ message: "invalid", onErr: "запрещено, Клиент не пренадлежит пользователю" })
+            }
+
+            const loc = await fastify.prisma.client.findFirst({
+                where: { id: Number(id) },
+                select: { serverName: true }
+            })
+
+            fastify.log.info(`Удаляем клиента ID=${id}, локация: ${loc.serverName}`)
+
+            await fastify.prisma.client.delete({
+                where: { id: Number(id) }
+            })
+
+            await syncwg(fastify, loc.serverName)
+
+            return reply.send({ message: "valid" })
+        }
+        catch (e) {
+            fastify.log.error(`Ошибка при удалении WG клиента: ${e}`)
+            return reply.send({ message: "invalid", e })
+        }
+    })
 }
--- dark-back/wg-serv/utils/defaultIp.js
+++ dark-back/wg-serv/utils/defaultIp.js
@@ -1,4 +1,5 @@
 export default async function defIp(fastify, serverName) {
+    fastify.log.info(`📥 [defIp] Запрос IP для сервера: ${serverName}`)
     const serverDb = await fastify.prisma.server.findFirst({
         where: { 
             serverName: serverName  // Имя сервера, переданное в функцию
@@ -7,9 +8,15 @@ export default async function defIp(fastify, serverName) {
             lan: true
         }    
     })
+    fastify.log.info(`📦 [defIp] Получен LAN из БД: ${serverDb.lan}`)
+
     const serverIp = serverDb.lan.replace(/\/.*/, '').replace(/"/g, '') // Удаляем все после "/" и все кавычки
+    
+    fastify.log.info(`🛠️ [defIp] IP без маски и кавычек: ${serverIp}`)
+    
     let [oc1, oc2, oc3, oc4] = serverIp.split(".").map(Number)
     oc4++
     let ipServ = `${oc1}.${oc2}.${oc3}.${oc4}`
+    fastify.log.info(`✅ [defIp] Сгенерированный IP: ${ipServ}`);
     return ipServ;
 }
