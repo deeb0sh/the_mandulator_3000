dbec1e19f8c12b29f1b528ed036caf196f0e35eb
deeb0sh <deeb0sh@ya.ru>
2025-05-01 13:14:22 +0300

    save all 2343454545

---
+++ dark-back/api-gateway/src/server.mjs
@@ -0,0 +1,48 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors';
+
+const fastify = Fastify({ logger: true })
+
+// CORS — только для darksurf.ru
+const allowedOrigins = ['https://darksurf.ru'];
+await fastify.register(cors, {
+  origin: (origin, cb) => {
+    if (origin && allowedOrigins.includes(origin)) {
+      cb(null, true);
+    } else {
+      cb(new Error(' доступ запрещён , пожалуйста пройдите нахуй '), false);
+    }
+  }
+});
+
+// Проксируем /auth/*
+fastify.all('/auth/*', async (req, reply) => {
+  const targetUrl = `http://localhost:3000${req.raw.url}`
+  const response = await fetch(targetUrl, {
+    method: req.method,
+    headers: { ...req.headers },
+    body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
+  })
+
+  reply.status(response.status)
+  const data = await response.text()
+  return reply.send(data)
+})
+
+// Проксируем /wg/*
+fastify.all('/wg/*', async (req, reply) => {
+  const targetUrl = `http://localhost:3001${req.raw.url}`
+  const response = await fetch(targetUrl, {
+    method: req.method,
+    headers: { ...req.headers },
+    body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
+  })
+
+  reply.status(response.status)
+  const data = await response.text()
+  return reply.send(data)
+})
+
+fastify.listen({ port: 2999, host: '0.0.0.0' }, () => {
+    console.log('bff na 2999')
+});
