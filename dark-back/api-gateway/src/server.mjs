2fa776ced1df3f9f9e1732d1c682ccd3d0a3aef4
deeb0sh <deeb0sh@ya.ru>
2025-05-31 11:30:19 +0300

    save all 5565656

--- dark-back/api-gateway/src/server.mjs
+++ dark-back/api-gateway/src/server.mjs
@@ -5,9 +5,8 @@ const fastify = Fastify({ logger: true })
 
 // CORS — только для darksurf.ru
 fastify.addHook('onRequest', (request, reply, done) => {
-  const allowedOrigin = 'https://darksurf.ru';
-  //const allowedOrigin = 'http://localhost';
-  // 1. Разрешаем все GET/HEAD/OPTIONS запросы
+  const allowedOrigin = ['https://darksurf.ru', 'https://darksurf.ru']
+    // 1. Разрешаем все GET/HEAD/OPTIONS запросы
   if (['GET', 'HEAD', 'OPTIONS'].includes(request.method)) {
     return done();
   }
