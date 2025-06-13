78f48fde363127ba7206352955b7b746dfb36979
deeb0sh <deeb0sh@ya.ru>
2025-06-13 11:35:34 +0300

    save all 4554324535

--- dark-bot/src/server.mjs
+++ dark-bot/src/server.mjs
@@ -1,14 +1,36 @@
 import { Bot } from 'grammy';
 import { config } from 'dotenv';
+import Fastify from 'fastify';
 
-config();
+config(); // прочитать .env в текущеи каталоге
 
 const token = process.env.TELETOK;
 const gid = process.env.GID;
 
 const bot = new Bot(token);
+const fastify = Fastify({
+  logger: false
+});
 
-// bot.api.sendMessage(gid, 'я запущен');
+fastify.post('/bot/msg', async (request, reply) => {
+    try {
+        const { message } = request.body; 
+        bot.api.sendMessage(gid, message);
+    } 
+    catch (err) {
+        console.log('[BOT] Ошибка отправлки сообщения в телегу');
+    }
+});
 
+const start = async () => {
+    try {
+      await fastify.listen({ port: 3333, host: '0.0.0.0' });
+      await bot.start();
+    } catch (err) {
+      console.log(`[BOT] Ошибка - ${err}`);
+      process.exit(1);
+    }
+  }
 
-bot.start();
+start();
+
