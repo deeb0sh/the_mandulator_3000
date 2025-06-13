78eb077a8d33fad796f4e6ce754a093e81a2b67d
deeb0sh <deeb0sh@ya.ru>
2025-06-13 12:06:57 +0300

    save all 343443334434

--- dark-bot/src/server.mjs
+++ dark-bot/src/server.mjs
@@ -25,7 +25,9 @@ fastify.post('/bot/msg', async (request, reply) => {
 const start = async () => {
     try {
       await fastify.listen({ port: 3333, host: '0.0.0.0' });
+      console.log('[BOT] fastify стартовал')
       await bot.start();
+      console.log('[BOT] bot стартовал')
     } catch (err) {
       console.log(`[BOT] Ошибка - ${err}`);
       process.exit(1);
